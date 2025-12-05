c2c7acfd8d8bfb7598951ed59543dc263721cad8
Corey Spicer <Spicer@thinkments.com>
2025-12-05 11:26:44 -0600

    Fix publicDir to serve sitemap.xml and static files

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@
   import path from 'path';
 
   export default defineConfig({
+    publicDir: 'src/public',
     plugins: [react()],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
